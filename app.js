1845752e8db376ef87b8f6eeff47d5ccadc9c650
marblerose123-star <marblerose123@gmail.com>
2026-07-15 11:37:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,29 @@ function startCamera() {
 function stopCamera() {
     document.getElementById("status").textContent = "⚪監視停止";
 }
+
+function addHistory(text) {
+
+    const list = document.getElementById("history");
+
+    if (!list) return;
+
+    if (list.innerHTML.includes("まだありません")) {
+        list.innerHTML = "";
+    }
+
+    const li = document.createElement("li");
+
+    const now = new Date();
+
+    const time =
+        now.getHours().toString().padStart(2,"0")
+        + ":"
+        +
+        now.getMinutes().toString().padStart(2,"0");
+
+    li.textContent = time + "　" + text;
+
+    list.prepend(li);
+
+}
