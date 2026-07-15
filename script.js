fd295b8851425b127431d5c816135af6dafc58d0
marblerose123-star <marblerose123@gmail.com>
2026-07-15 11:53:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+function startMonitor() {
 
+    document.getElementById("status").textContent = "🟢監視中";
+
+    addHistory("🐱 テスト検知");
+
+}
+
+function stopMonitor() {
+
+    document.getElementById("status").textContent = "⚪監視停止";
+
+}
+
+function addHistory(text){
+
+    const history = document.getElementById("history");
+
+    if(history.innerHTML=="<li>まだありません</li>"){
+
+        history.innerHTML="";
+
+    }
+
+    const now = new Date();
+
+    const time =
+        now.getHours().toString().padStart(2,"0")
+        + ":"
+        +
+        now.getMinutes().toString().padStart(2,"0");
+
+    history.innerHTML =
+        "<li>"+time+" "+text+"</li>"
+        + history.innerHTML;
+
+}
