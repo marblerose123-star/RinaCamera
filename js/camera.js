b9966b2ec507c03032e17d5b83523943f0bec824
marblerose123-star <marblerose123@gmail.com>
2026-07-16 11:31:45 +0900

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -5,6 +5,12 @@ function cameraReady() {
 
     console.log("Camera Module Ready");
 
+    const camera =
+    document.getElementById("cameraStatus");
+
+    camera.textContent =
+    "📷 カメラ：接続準備完了";
+
     return true;
 
 }
