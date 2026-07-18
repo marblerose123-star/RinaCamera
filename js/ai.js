b9606d773fee79defce7830d3eb65762f5e187d6
marblerose123-star <marblerose123@gmail.com>
2026-07-18 09:55:36 +0900

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -4,25 +4,11 @@ function aiReady(){
     console.log("AI Module Ready");
 
 }
+
 function detectObject(){
 
-    console.log("AIで画像を解析中...");
-
-    const testObjects = [
-
-        "chacha",
-
-        "shiro",
-
-        "person",
-
-        "none"
-
-    ];
-
-    const random =
-        Math.floor(Math.random()*testObjects.length);
-
-    return testObjects[random];
+    return "chacha";
 
 }
+
+    
