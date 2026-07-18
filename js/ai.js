2a073209b1a34b503472dbc914177110c23b8207
marblerose123-star <marblerose123@gmail.com>
2026-07-18 12:30:55 +0900

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -5,21 +5,26 @@ function aiReady(){
 
 }
 
-function detectObject(){
+function detectObject(image){
 
     console.log("AIで画像を解析中...");
 
+    console.log("解析画像:", image);
+
     const testObjects = [
+
         "chacha",
+
         "shiro",
+
         "person",
+
         "none"
+
     ];
 
     const random =
-        Math.floor(Math.random() * testObjects.length);
-
-    console.log("AI判定:", testObjects[random]);
+        Math.floor(Math.random()*testObjects.length);
 
     return testObjects[random];
 
