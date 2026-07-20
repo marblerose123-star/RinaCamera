bbaeeba3a22b0eb51e00af25da2e140200e8e5f1
marblerose123-star <marblerose123@gmail.com>
2026-07-20 17:08:28 +0900

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -1,15 +1,15 @@
 // Rina Camera
 // Camera Control
 
-function cameraReady() {
+function cameraReady(){
 
     console.log("Camera Module Ready");
 
     const camera =
-    document.getElementById("cameraStatus");
+        document.getElementById("cameraStatus");
 
     camera.textContent =
-    "📷 カメラ：接続準備完了";
+        "📷 カメラ：接続準備完了";
 
     return true;
 
@@ -19,18 +19,19 @@ function getCameraImage(){
 
     console.log("カメラ画像を取得");
 
-    return "images/chacha-test.jpg";
+    const testImages = [
 
-}
+        "images/chacha-test.jpg",
 
-const random =
-    Math.floor(Math.random()*testImages.length);
+        "images/shiro-test.jpg",
 
-const live =
-    document.getElementById("liveCamera");
+        "images/person-test.jpg"
 
-live.src = testImages[random];
+    ];
 
-return testImages[random];
+    const random =
+        Math.floor(Math.random() * testImages.length);
+
+    return testImages[random];
 
 }
