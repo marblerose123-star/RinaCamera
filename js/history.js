822210ba31f58d705792f36285554f94b785c034
marblerose123-star <marblerose123@gmail.com>
2026-07-28 12:31:15 +0900

    Add loadHistory and saveHistory functions
    Implement functions to load and save history to localStorage.

--- js/history.js
+++ js/history.js
@@ -1,1 +1,28 @@
+// ------------------------------
+// History Engine
+// ------------------------------
+
+function loadHistory() {
+
+    const saved = localStorage.getItem("rinaHistory");
+
+    if(saved){
+
+        document.getElementById("history").innerHTML = saved;
+
+    }
+
+}
+
+function saveHistory(){
+
+    localStorage.setItem(
+
+        "rinaHistory",
+
+        document.getElementById("history").innerHTML
+
+    );
+
+}
 
